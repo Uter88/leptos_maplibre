55101a8e9911ef1e85b5a2db5319762b35ae00c6
Uter <64920341+Uter88@users.noreply.github.com>
2026-05-15 13:38:33 +0300

    Update map.js

--- crates/leptos_maplibre/bindings/js/src/map.js
+++ crates/leptos_maplibre/bindings/js/src/map.js
@@ -1,4 +1,4 @@
-import maplibregl from "https://esm.sh/maplibre-gl@v6.0.0-9";
+import maplibregl from "https://esm.sh/maplibre-gl@5.24.0";
 let next_id = 1;
 let next_marker_id = 1;
 let next_popup_id = 1;
